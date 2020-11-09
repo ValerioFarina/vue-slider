7728718d047560d1d109cd95d4e20ddb1fd114a8
Valerio Farina <valerio.farina.1994@gmail.com>
2020-11-09 15:42:31 +0100

    Fatto in modo che le immagini "scorrano" verso destra automaticamente ogni 3 secondi. Questo "scorrimento" automatico viene stoppato al click di una delle due frecce

--- main.js
+++ main.js
@@ -33,6 +33,18 @@ var app = new Vue({
             if (this.imgIndex > this.images.length - 1) {
                 this.imgIndex = 0;
             }
+        },
+        // this function will be called at the click of one of the carousel's arrows
+        // the invocation of this function will stop the carousel's autoplay
+        stop() {
+            clearInterval(this.clock);
         }
+    },
+    // this function is called after the Vue's instance is created
+    // this function sets an interval of 3 seconds
+    // every 3 seconds the function next is called, and thus the carousel's images "scroll" from left to right automatically every 3 seconds
+    // the reference to the interval is saved in the variable "clock", which will be a key within the data object
+    created : function() {
+        this.clock = setInterval(this.next, 3000);
     }
 });
