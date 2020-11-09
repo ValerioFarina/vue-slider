841deca602b691519b6532fcf2687211eb420cf0
Valerio Farina <valerio.farina.1994@gmail.com>
2020-11-09 14:34:53 +0100

    Impostato carosello statico

---
+++ main.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+    el : '#root',
+
+    data : {
+        images : [
+            'img/img1.jpg',
+            'img/img2.jpg',
+            'img/img3.jpg',
+            'img/img4.jpg'
+        ],
+        
+        imgIndex : 0
+    },
+
+    methods : {
+
+    }
+});
