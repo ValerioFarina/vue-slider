7c68ced34bc1842a3cf037e8bf011aec51474406
Valerio Farina <valerio.farina.1994@gmail.com>
2020-11-10 18:07:32 +0100

    Fatto in modo che l'utente possa inserire una nuova immagine ed eliminare l'immagine corrente

--- main.js
+++ main.js
@@ -13,7 +13,11 @@ var app = new Vue({
             'img/img6.jpg'
         ],
         // imgIndex represents the index (within the array images) of the image currently displayed in the carousel
-        imgIndex : 0
+        imgIndex : 0,
+        // the value of newImg will the url inserted by the user
+        newImg : '',
+        // the value of inputMessage is the text displayed in the input before the user insert the url of an image
+        inputMessage : 'Insert an image URL'
     },
 
     methods : {
@@ -41,6 +45,37 @@ var app = new Vue({
         // the invocation of this function will stop the carousel's autoplay
         stop() {
             clearInterval(this.clock);
+        },
+        // this function adds a new image to the carousel
+        // the function will be called at the click of the coresponding button
+        addNewImg() {
+            // every time the button is clicked, we set inputMessage equal to "Insert an image URL"
+            this.inputMessage = 'Insert an image URL';
+            // if the image url inserted by the user was not previously inserted by the user himself,
+            // and if the user input is not an empty string,
+            if (!this.images.includes(this.newImg) && this.newImg != '') {
+                // then we push the url in the array images
+                this.images.push(this.newImg);
+                // moreover, we reset newImg (i.e. the value of the input)
+                this.newImg = '';
+            // else, if the url has already been inserted
+            } else if (this.images.includes(this.newImg)) {
+                // we reset newImg (i.e. the value of the input)
+                this.newImg = '';
+                // moreover, we notify the user
+                this.inputMessage = 'WARNING : This image has already been inserted';
+            }
+        },
+        // this function deletes the currently displayd image
+        // the function will be called at the click of the corresponding button
+        deleteImg() {
+            let leftSlice = this.images.slice(0, this.imgIndex);
+            let rightSlice = this.images.slice(this.imgIndex + 1);
+            this.images = leftSlice.concat(rightSlice);
+            // if the deleted image was the last one, we have to decrement imgIndex
+            if (this.imgIndex == this.images.length) {
+                this.imgIndex--;
+            }
         }
     },
 
